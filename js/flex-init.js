804f8edf316e2e3caf305aaecf93dbd0cd981ad6
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-02-14 09:11:12 +0000

    Set the controls up for the script

---
+++ js/flex-init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(document).ready(function() {
+    $('.flexlsider').flexlsider({
+      controlNav: false,
+      directionNav: false,
+      animation: "fade"
+    });
+  });
+})(jQuery);
