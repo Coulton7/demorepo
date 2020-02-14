1ba95cc2e193a905470b9f57a2fee099f0b07565
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-02-14 15:38:04 +0000

    Adjusting script and class to add overflow hidden y

--- js/nav-btn.js
+++ js/nav-btn.js
@@ -4,8 +4,10 @@ function navBtn(x) {
     if (x.style.height === "100%") {
         x.style.height = "0%";
         $('.main').removeClass('active');
+        $("body").removeClass('noscroll');
       } else {
           x.style.height = "100%";
           $('.main').addClass('active');
+          $("body").addClass('noscroll');
       }
   }
