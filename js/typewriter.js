594f2fd1c9bf067b83e6060d590ddb0ea13af05e
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-02-20 12:37:23 +0000

    Also found way to prevent constant looping text

--- js/typewriter.js
+++ js/typewriter.js
@@ -2,7 +2,7 @@ var TxtRotate = function(el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
   this.loopNum = 0;
-  this.period = parseInt(period, 10) || 400;
+  this.period = parseInt(period, 10) || 2000;
   this.txt = '';
   this.tick();
   this.isDeleting = false;
@@ -29,7 +29,7 @@ TxtRotate.prototype.tick = function() {
     delta = this.period;
     this.isDeleting = true;
   } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
+    this.isDeleting = true;
     delta = 500;
   }
 
