226c04729322c922bca8fcbe484c65c80401505a
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-02-20 10:39:55 +0000

    had the incorrect classes on the js and added class for typewrite

--- js/typewriter.js
+++ js/typewriter.js
@@ -40,12 +40,17 @@ TxtRotate.prototype.tick = function() {
 };
 
 window.onload = function() {
-  var elements = document.getElementsByClassName('txt-rotate');
+  var elements = document.getElementsByClassName('typewrite');
   for (var i=0; i<elements.length; i++) {
-    var toRotate = elements[i].getAttribute('data-rotate');
+    var toRotate = elements[i].getAttribute('data-type');
     var period = elements[i].getAttribute('data-period');
     if (toRotate) {
       new TxtRotate(elements[i], JSON.parse(toRotate), period);
     }
   }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
 };
