f59dffaf1a8a97e1b197e1bc493661f1e629ca68
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-02-20 15:05:28 +0000

    Removing after can't apply it on slides them selves so new tatic required

--- js/flex-init.js
+++ js/flex-init.js
@@ -4,17 +4,7 @@
       controlNav: false,
       directionNav: false,
       animation: "fade",
-      before: function() {},
-      after: function() {
-          var elements = document.getElementsByClassName('flex-active-slide').getElementsByClassName('typewrite');
-          for (var i = 0; i < elements.length; i++) {
-            var toRotate = elements[i].getAttribute('data-type');
-            var period = elements[i].getAttribute('data-period');
-            if (toRotate) {
-              new TxtRotate(elements[i], JSON.parse(toRotate), period);
-            }
-          }
-      }
+      slideshowSpeed: 10000
     });
   });
 })(jQuery);
