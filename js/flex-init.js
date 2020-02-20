f23f2b425924b9f243403fe6bceeeb761d6be9a6
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-02-20 14:40:31 +0000

    a little bit of format and an attempt at if statement for typewriter

--- js/flex-init.js
+++ js/flex-init.js
@@ -1,17 +1,19 @@
-(function($){
+(function($) {
   $(document).ready(function() {
     $('.flexslider').flexslider({
       controlNav: false,
       directionNav: false,
       animation: "fade",
-      before: function(){},
+      before: function() {},
       after: function() {
-        var elements = document.getElementsByClassName('typewrite');
-        for (var i=0; i<elements.length;i++) {
-          var toRotate = elements[i].getAttribute('data-type');
-          var period = elements[i].getAttribute('data-period');
-          if (toRotate) {
-            new TxtRotate(elements[i], JSON.parse(toRotate), period);
+        if (document.getElementsByClassName('flex-active-slide')) {
+          var elements = document.getElementsByClassName('typewrite');
+          for (var i = 0; i < elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtRotate(elements[i], JSON.parse(toRotate), period);
+            }
           }
         }
       }
