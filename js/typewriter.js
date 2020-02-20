9f993e9d013c493287bbbf34dcdefbe908d76acc
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-02-20 15:11:40 +0000

    reset the script mostly

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,14 +1,14 @@
-var TxtRotate = function(el, toRotate, period) {
+var TxtType = function(el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
   this.loopNum = 0;
-  this.period = parseInt(period, 10) || 10000;
+  this.period = parseInt(period, 10) || 2000;
   this.txt = '';
   this.tick();
   this.isDeleting = false;
 };
 
-TxtRotate.prototype.tick = function() {
+TxtType.prototype.tick = function() {
   var i = this.loopNum % this.toRotate.length;
   var fullTxt = this.toRotate[i];
 
@@ -18,18 +18,21 @@ TxtRotate.prototype.tick = function() {
     this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
 
-  this.el.innerHTML = '<div class="typewriter">'+this.txt+'</div>';
+  this.el.innerHTML = '<div class="typewriter">' + this.txt + '</div>';
 
   var that = this;
-  var delta = 300 - Math.random() * 100;
+  var delta = 200 - Math.random() * 100;
 
-  if (this.isDeleting) { delta /= 2; }
+  if (this.isDeleting) {
+    delta /= 2;
+  }
 
   if (!this.isDeleting && this.txt === fullTxt) {
     delta = this.period;
     this.isDeleting = true;
   } else if (this.isDeleting && this.txt === '') {
     this.isDeleting = false;
+    this.loopNum++;
     delta = 500;
   }
 
@@ -40,11 +43,11 @@ TxtRotate.prototype.tick = function() {
 
 window.onload = function() {
   var elements = document.getElementsByClassName('typewrite');
-  for (var i=0; i<elements.length;i++) {
+  for (var i = 0; i < elements.length; i++) {
     var toRotate = elements[i].getAttribute('data-type');
     var period = elements[i].getAttribute('data-period');
     if (toRotate) {
-      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+      new TxtType(elements[i], JSON.parse(toRotate), period);
     }
   }
 };
