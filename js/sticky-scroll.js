a5c8f7bae6a01fc3ce3b21f6009094f62e2ac1a0
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2020-02-21 10:08:42 +0000

    Setting the classes up for the full screen nav

--- js/sticky-scroll.js
+++ js/sticky-scroll.js
@@ -2,6 +2,7 @@
 
   $(document).ready(function() {
     "use strict";
+    var colNav = $('.col-nav');
     var nav = $('.navbar'),
       body = $('body'),
       windowScreen = $(window),
@@ -10,6 +11,8 @@
 
     if (windowScreen.scrollTop() >= navOffset && !nav.hasClass('sticky')) {
       nav.addClass('sticky');
+      colNav.removeClass('col-sm-8 col-sm-offset-2');
+      colNav.addClass('col-sm-12');
       $(body).css({
         paddingTop: nav.css('height')
       });
@@ -18,6 +21,8 @@
 
       if (windowScreen.scrollTop() >= navOffset && !nav.hasClass('sticky')) {
         nav.addClass('sticky');
+        colNav.removeClass('col-sm-8 col-sm-offset-2');
+        colNav.addClass('col-sm-12');
         $(body).css({
           paddingTop: nav.css('height')
         });
@@ -25,6 +30,8 @@
 
       if (windowScreen.scrollTop() < navOffset && nav.hasClass('sticky')) {
         nav.removeClass('sticky');
+        colNav.addClass('col-sm-8 col-sm-offset-2');
+        colNav.removeClass('col-sm-12');
         $(body).css({
           paddingTop: 0
         });
